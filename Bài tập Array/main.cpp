f7f05a3522878a241f56594107ebce1ec92c6191
Kevin <trungkien02005a@gmail.com>
2026-08-20 14:32:48 +0700

    edit

--- Bài tập Array/main.cpp
+++ Bài tập Array/main.cpp
@@ -22,11 +22,11 @@ void program_push_back(int a[], int &lenght, int x)
     lenght++;
 }
 
-void program_insert(int a[], int &lenght, int x, int position)
+void program_insert(int a[], int &lenght, int position, int x)
 {
     if (position < 0 || position > lenght)
     {
-        std::cout << "Array no chèn được";
+        std::cout << "array no";
         return;
     }
     for (int i = lenght; i >= position; i--)
@@ -37,9 +37,9 @@ void program_insert(int a[], int &lenght, int x, int position)
     lenght++;
 }
 
-void program_front(int a[], int &lenght, int x)
+void program_push_front(int a[], int &lenght, int x)
 {
-    program_insert(a, lenght, x, 0);
+    program_insert(a, lenght, 0, x);
 }
 
 void program_remove(int a[], int &lenght, int position)
@@ -47,21 +47,49 @@ void program_remove(int a[], int &lenght, int position)
     for (int i = position; i <= lenght; i++)
     {
         a[i] = a[i + 1];
-        }
+    }
     lenght--;
 }
+
+void remove_values(int a[], int &lenght, int x)
+{
+    for (int i = 0; i < lenght; i++)
+    {
+        while (a[i] == x)
+        {
+            program_remove(a, lenght, i);
+        }
+    }
+}
+
 int main()
 {
     int a[Max];
     int lenght = 0;
-    // program_print(a, lenght);
 
+    std::cout << "1 - Test push back: ";
     program_push_back(a, lenght, 5);
     program_push_back(a, lenght, 10);
     program_push_back(a, lenght, 15);
     program_push_back(a, lenght, 20);
-    program_insert(a, lenght, 100, 3);
-    program_front(a, lenght, 200);
-    program_remove(a, lenght, 1);
+    program_print(a, lenght);
+
+    std::cout << "2 - Test insert: ";
+    program_insert(a, lenght, 2, 13);
+    program_print(a, lenght);
+
+    std::cout << "3 - Test push front: ";
+    program_push_front(a, lenght, 2);
+    program_push_front(a, lenght, 2);
+    program_push_front(a, lenght, 2);
+    program_print(a, lenght);
+
+    std::cout << "4 - Test remove: ";
+    program_remove(a, lenght, 4);
+    program_print(a, lenght);
+
+    std::cout << "5 - test remove value: ";
+    remove_values(a, lenght, 2);
+    remove_values(a, lenght, 15);
     program_print(a, lenght);
 }
